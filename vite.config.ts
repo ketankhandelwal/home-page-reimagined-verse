7951414f64df8ade76147cc2e4340ac90f016104
ketankhandelwal <ketankhandelwal70@gmail.com>
2025-06-08 17:40:48 +0530

    fixes

--- vite.config.ts
+++ vite.config.ts
@@ -23,6 +23,7 @@ export default defineConfig(({ mode }) => ({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
+    sourcemap: false,
     rollupOptions: {
       output: {
         assetFileNames: (assetInfo) => {
